9ed955b5f1c726a29d5f0566940076ced72d750f
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2023-02-22 19:41:10 +0300

    Database Updated

--- app.js
+++ app.js
@@ -1,59 +1,77 @@
-const path = require('path')
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
-const hostname = '127.0.0.1'
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
-const generateDate = require('./helpers/generateDate').generateDate
-const expressSession = require('express-session')
-const connectMongo = require('connect-mongo')
-const methodOverride = require('method-override')
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+const hostname = "127.0.0.1";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const generateDate = require("./helpers/generateDate").generateDate;
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
+const methodOverride = require("method-override");
+const dotenv = require("dotenv");
 
-mongoose.connect('mongodb://127.0.0.1/sorce_db')
+dotenv.config();
+function conn() {
+  mongoose
+    .connect(process.env.DB_URI, {
+      dbName: "sorce_db",
+    })
+    .then(() => {
+      console.log("Connet to DB");
+    })
+    .catch((err) => {
+      console.log(`DB connection err: ${err}`);
+    });
+}
+conn();
 
-const mongoStore = connectMongo(expressSession)
+const mongoStore = connectMongo(expressSession);
 
-app.use(expressSession({
-    secret: 'testotesto',
+app.use(
+  expressSession({
+    secret: "testotesto",
     resave: false,
     saveUninitialized: true,
-    store: new mongoStore({ mongooseConnection: mongoose.connection })
-}))
+    store: new mongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 //Flash - Message Middleware
 app.use((req, res, next) => {
-    res.locals.sessionFlash = req.session.sessionFlash
-    delete req.session.sessionFlash
-    next()
-})
+  res.locals.sessionFlash = req.session.sessionFlash;
+  delete req.session.sessionFlash;
+  next();
+});
 
-app.use(fileUpload())
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
-app.engine('handlebars', exphbs.engine({ helpers: { generateDate: generateDate } }))
-app.set('view engine', 'handlebars')
+app.engine(
+  "handlebars",
+  exphbs.engine({ helpers: { generateDate: generateDate } })
+);
+app.set("view engine", "handlebars");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const main = require('./routes/main')
-const posts = require('./routes/posts')
-const users = require('./routes/users')
-const admin = require('./routes/admin/index')
+const main = require("./routes/main");
+const posts = require("./routes/posts");
+const users = require("./routes/users");
+const admin = require("./routes/admin/index");
 
-app.use('/', main)
-app.use('/posts', posts)
-app.use('/users', users)
-app.use('/admin', admin)
-
+app.use("/", main);
+app.use("/posts", posts);
+app.use("/users", users);
+app.use("/admin", admin);
 
 app.listen(port, hostname, () => {
-    console.log(`Server çalışıyor, http://${hostname}:${port}`);
+  console.log(`Server çalışıyor, http://${hostname}:${port}`);
 });
